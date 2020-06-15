cf3153151eb4b0b8c362d1f11c337b8e95cedc62
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-06-15 16:11:47 -0600

    added react

--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,2 +1,32 @@
 class Api::MenusController < ApplicationController
+  def index
+    render json: Menu.all
+  end
+
+  def create
+    menu = Menu.new(menu_params)
+    if menu.save
+      render json: menu
+    else
+      render json: { errors: menu.errors }, status :unprocessable_entity 
+    end
+  end
+
+  def update
+    menu = Menu.find(params[:id])
+    menu.update
+    render json: menu
+  end
+
+  def destroy
+    Menu.find(params[:id]).destroy
+    render json: { message: 'Menu deleted' }
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
+
 end
